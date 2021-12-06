94d46d965071979671e264120958bddabba7fd20
Clark Farriss <clarkfarriss@Clarks-MBP.lan>
2021-12-06 11:01:44 -0500

    creating seperate product routes / handling

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import Product from '../models/productModel.js'
+import asyncHander from 'express-async-handler'
+const router = express.Router()
+
+
+// @desc     Fetch all products
+// @route    GET /api/products
+// @access   Public
+router.get('/', asyncHander(async(req,res) => {
+    const products = await Product.find({})
+    res.json(products)
+}))
+
+
+// @desc     Fetch single product
+// @route    GET /api/products/:id
+// @access   Public
+router.get('/:id', asyncHander(async(req,res) => {
+    const product = await Product.findById(req.params.id)
+
+    if(product){
+        res.json(product)
+    } else {
+        res.status(404).json({ message: 'Product not found' })
+    }
+    
+}))
+
+export default router
