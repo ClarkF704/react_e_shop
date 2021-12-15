7742ae1e941945e3389f17b75a9dafb29dbf7d02
Clark Farriss <clarkfarriss@Clarks-MBP.lan>
2021-12-15 16:57:30 -0500

    quantity select and add to cart btn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import CartScreen from './screens/CartScreen';
 import ProductScreen from './screens/ProductScreen';
 import { Container } from 'react-bootstrap';
 
@@ -14,6 +15,7 @@ const App = () => {
            <Container>
               <Route path='/' component={HomeScreen} exact />
               <Route path='/product/:id' component={ProductScreen} />
+              <Route path='/cart/:id?' component={CartScreen} />
           </Container>
         </main>
      
