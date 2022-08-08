8004d171d141aba4eb7d893690c30c2487b63e37
Clark Farriss <clarkfarriss@Clarks-MBP.lan>
2022-08-08 14:34:31 -0400

    updating uer endpoint to get and put

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,27 @@
-import express from 'express'
-import {authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser} from '../controllers/userController.js'
-const router = express.Router()
-import {protect, admin} from '../middleware/authMiddleware.js'
+import express from 'express';
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from '../controllers/userController.js';
+const router = express.Router();
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').post(registerUser).get(protect, admin, getUsers)
-router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers);
+router.post('/login', authUser);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
-export default router
+export default router;
