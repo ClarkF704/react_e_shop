9c6ff62e7a1396c0434563477da73faed001abb0
Clark Farriss <clarkfarriss@Clarks-MBP.lan>
2022-08-22 13:20:06 -0400

    adding carousel

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,6 +7,7 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import Paginate from '../components/Paginate'
 import {listProducts} from '../actions/productActions'
+import ProductCarousel from '../components/ProductCarousel'
 
 const HomeScreen = ({ match }) => {
 
@@ -25,6 +26,7 @@ const HomeScreen = ({ match }) => {
     
     return (
         <>
+        {!keyword && <ProductCarousel /> }
            <h1>Latest Products</h1>
            {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : 
            <>
